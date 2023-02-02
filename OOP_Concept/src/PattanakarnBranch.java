4f0879db5c9304417d7295fb5f8d95a22ac0a90e
Apichai-April <hana009x@gmail.com>
2023-02-02 10:24:56 +0700

    EXAMPLE

---
+++ OOP_Concept/src/PattanakarnBranch.java
@@ -0,0 +1,27 @@
+
+public class PattanakarnBranch extends Product {
+	
+	private int payUnit() {
+		return  super.getUnit() / 2;
+
+	}
+	
+	private int freeUnit() {
+		return super.getUnit() - this.payUnit();
+	}
+	
+	public int getTotalprice() {
+		return this.payUnit() * 100;
+	}
+	
+	public String toString() {
+		return "You buy "+ payUnit()+" units, get free " +freeUnit()+" units("+getTotalprice()+")";
+	}
+	
+	
+	
+	
+	
+	
+	
+}
