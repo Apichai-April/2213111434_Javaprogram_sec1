0bbe93cff72cbeb2d73ac0d7ad22f28ddcf7ba1e
Apichai-April <hana009x@gmail.com>
2023-02-16 09:06:52 +0700

    Attendance16/02/2023

--- OOP_Concept/src/ShapeDemo1.java
+++ OOP_Concept/src/ShapeDemo1.java
@@ -1,8 +1,26 @@
-
+import java.util.*;
 public class ShapeDemo1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print("Input radius = ");
+		double radius = scan.nextDouble();
+		System.out.print("Input height = ");
+		double height = scan.nextDouble();
+		
+		Circle circle = new Circle(radius,null);
+		System.out.print("Circle ["+ circle +"]");
+		System.out.print("Circle [+ area =" + circle.getArea() + "]");	
+		System.out.println();
+		
+		Cylinder cylinder = new Cylinder(radius, height);
+		System.out.print("Cylinder[height = "+ cylinder +"]");
+		System.out.print("Cylinder[area   = "+ cylinder.getArea()+"]");
+		System.out.print("Cylinder[volume = "+ cylinder.getVolume() +"]");
+		
+	
 
 	}
 
